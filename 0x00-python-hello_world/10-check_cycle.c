6887fde0d42d3bd7b53cc5f92c478ea4bfb7ca4a
Carmel Prosper SAGBO <sagbocarmel@gmail.com>
2022-06-01 02:47:10 +0100

    Python hello world

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -9,19 +9,20 @@
  */
 int check_cycle(listint_t *list)
 {
-    listint_t *slow = list;
-    listint_t *fast = list;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-    while (fast != NULL && fast->next != NULL)
-    {
-        slow = slow->next;
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
 
-        fast = fast->next->next;
+		fast = fast->next->next;
 
-        if (slow == fast) {
-            return 1;
-        }
-    }
+		if (slow == fast)
+		{
+			return (1);
+		}
+	}
 
-    return 0;
+	return (0);
 }
