ebc2665d1786ef3f9d7dc3c60e482ca7984a64a3
Carmel Prosper SAGBO <sagbocarmel@gmail.com>
2022-06-21 03:57:10 +0100

    File check

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    response = 0
+    res = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-            response += 1
+            res += 1
         except IndexError:
             break
     print("")
-    return (response)
+    return (res)
