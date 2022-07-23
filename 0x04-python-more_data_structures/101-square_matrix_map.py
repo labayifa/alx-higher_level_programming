45b71787c86fa8af5f974aafa7c0df7a6c12d037
Carmel Prosper SAGBO <sagbocarmel@gmail.com>
2022-07-23 14:35:46 +0100

    More data structure

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    """
-    Matrix square return
-    :param matrix:
-    :return:
-    """
     return (list(map(lambda x: list(map(lambda y: y ** 2, x[:])), matrix)))
