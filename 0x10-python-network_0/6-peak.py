767268080c2dac70c66303a8835a3d8f3f55b52e
Carmel Prosper SAGBO <sagbocarmel@gmail.com>
2022-09-22 23:01:47 +0100

    Request with curl

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python3
-# This script sends a request to that URL, and displays the size of the body of the response
-echo "$1"
-curl -s "$1" -w size_request
+"""Defines a peak-finding algorithm."""
+
+
+def find_peak(list_of_integers):
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
+        return None
+
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
+
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
+
 
