7d80e4b1f05e5bfd3f515fa83260d2ffbb434258
Carmel Prosper SAGBO <sagbocarmel@gmail.com>
2022-09-28 00:19:46 +0100

    Close Github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Get Github User Id based on username and password.
+
+Usage: ./10-my_github.py <username> <pat>
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+
+    results = requests.get(url, auth=(sys.argv[1], sys.argv[2])).json()
+    if results.get('id') is None:
+        print("None")
+    else:
+        print("{}".format(results.get('id')))
+
--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Get Github commits for a repo and owner.
+
+Usage: ./100-github_commits.py <repo> <owner>
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
